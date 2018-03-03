cfab153cb32e054e2a4d8f76cc0028568d9e9771
Steve Walker <captnwalker@gmail.com>
2018-03-03 10:52:54 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { startLogout } from '../actions/auth';
+
+
+export const Header = ({ startLogout }) => (
+    
+    <header className="header">
+        
+        <div className="content-container"> 
+
+            <div className="header__content">
+
+                <Link className="header__logo" to="/dashboard">
+                     <h1>Restaurant Expense Tracker</h1>
+                </Link>
+
+                <a href ="/reservationspage" button className="button button--link">Reservations</a>
+
+
+             <button className="button button--link" onClick={startLogout}>Logout</button>
+        </div>
+   </div>
+</header>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
